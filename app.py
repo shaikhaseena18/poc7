ed29d1066db68ec194c6a5f1ca551387b4ceb755
shaikhaseena18 <shaik-haseena@hcltech.com>
2026-04-28 14:05:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello from POC-7 on Amazon Linux")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from POC-7 running in browser 🚀"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
